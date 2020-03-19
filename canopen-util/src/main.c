b36eec5459eb974245c640ae21c25698ec3850af
cesar romero <romero@braincorporaiton.com>
2020-03-18 21:51:31 -0700

    adding object dictionary files and initial CMakeLists.txt

---
+++ canopen-util/src/main.c
@@ -0,0 +1,20 @@
+//==================================================================
+// Copyright 2020 Brain Corporation. All rights reserved. Brain
+// Corporation proprietary and confidential.
+// ALL ACCESS AND USAGE OF THIS SOURCE CODE IS STRICTLY PROHIBITED
+// WITHOUT EXPRESS WRITTEN APPROVAL FROM BRAIN CORPORATION.
+// Portions of this Source Code and its related modules/libraries
+// may be governed by one or more third party licenses, additional
+// information of which can be found at:
+// https://info.braincorp.com/open-source-attributions
+//==================================================================
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+    printf("canopen-util application");
+
+
+    return 0;
+}
