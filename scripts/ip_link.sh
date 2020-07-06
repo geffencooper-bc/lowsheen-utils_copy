453c2abea548eb7ae1930643913fcb338f1319b7
cesar romero <romero@braincorporaiton.com>
2020-07-05 17:30:11 -0700

    change install directory from custom path to usr/local/bin path. fix can1 to can2 reference

--- scripts/ip_link.sh
+++ scripts/ip_link.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+# helper script to bring up the socket can interfaces manually
 
 ip link set down can0
-ip link set down can1
+ip link set down can2
 
 ip link set can0 type can bitrate 250000
-ip link set can1 type can bitrate 250000
+ip link set can2 type can bitrate 250000
 
 ip link set up can0
-ip link set up can1
+ip link set up can2
 
 
