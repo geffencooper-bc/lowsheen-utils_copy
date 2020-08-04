4eabc1cce2d1c0cbe2f0dc96e1598705624452ed
Geffen <geffen.cooper@braincorp.com>
2020-08-04 13:10:37 -0700

    appended apache license

--- scripts/ip_link.sh
+++ scripts/ip_link.sh
@@ -8,6 +8,18 @@
 # may be governed by one or more third party licenses, additional
 # information of which can be found at:
 # https://info.braincorp.com/open-source-attributions
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # ============================================================================
 # helper script to bring up the socket can interfaces manually
 
